7ada894177f752d74dd898165bf3795ba47271a4
giuliacajati <giuliacajati@gmail.com>
2020-08-24 18:45:53 -0400

    submit

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def bark
+        puts ("Woof Woof \n")
+    end
+end
+
+# my dogs   
+fido = Dog.new 
+snoopy = Dog.new 
+lassie = Dog.new 
+
+
+fido.bark
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    def talk 
+        print "Hello world!"
+    end
+end 
+
+adele_goldberg = Person.new
+alan_kay = Person.new
